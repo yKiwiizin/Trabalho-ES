ae5edebdc8da0bac3ea25d07a434e10fcdae3576
yKiwiizin <otaviooo0805@gmail.com>
2025-08-10 04:32:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const openButtons = document.querySelectorAll('.container-animal .animais-slogan .titulo-animal .open-modal')
+
+openButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+
+        modal.showModal();
+    });
+});
+
+const closeButtons = document.querySelectorAll('.close-modal')
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalId = button.getAttribute('data-modal');
+        const modal = document.getElementById(modalId);
+
+        modal.close()
+    });
+});
+
+const header = document.getElementById('cabecalho');
+
+const scrollThreshold = 130; // Ponto de rolagem para diminuir o cabeçalho
+
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset >= scrollThreshold) {
+    header.classList.add('encolhido');
+    
+  } 
+  if (window.pageYOffset <= scrollThreshold-75) {
+    header.classList.remove('encolhido');
+    
+  } 
+});
